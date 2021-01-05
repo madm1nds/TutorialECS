01d2f571cdc87bf70cf4f5e69475d9188ecfcc8f
madm1nds <maxalexay1@gmail.com>
2021-01-05 16:00:57 +0200

    Added EcsStartup.cs and InitializeFieldSystem.cs

---
+++ Assets/Scripts/InitializeFieldSystem.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.Ecs;
+
+namespace TicToe
+{
+    internal class InitializeFieldSystem : IEcsInitSystem
+    {
+        public void Init()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
